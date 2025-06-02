02ba08117c007b6331063eb7b1fdba6ced0c065e
Eleina Hayden <github@lly.email>
2025-06-02 01:37:06 -0500

    Update github-ipset.py

--- python/github-ipset.py
+++ python/github-ipset.py
@@ -1,11 +1,12 @@
 # Script to add all GitHub IPs to an IPSet on PVE
 import requests
 import os
+import subproccess
 
 headers = {'Authorization': 'TOKEN'}
 response = requests.get('https://api.github.com/meta')
 data = response.json()
-cats = ['hooks', 'web', 'api', 'git', 'github_enterprise_importer', 'packages', 'pages', 'importer', 'actions', 'actions_macos', 'codespaces', 'dependabot', 'copilot'] # !! simplify this eventually instead of needing to list out each one !!
+cats = ['hooks', 'web', 'api', 'git', 'github_enterprise_importer', 'packages', 'pages', 'importer', 'actions', 'actions_macos', 'codespaces', 'dependabot', 'copilot'] # TODO: simplify this eventually instead of needing to list out each one
 
 ips = []
 
@@ -16,5 +17,5 @@ for cat in cats:
 ips = list(set(ips))
 
 for ip in ips:
-    cmd = f'pvesh create /cluster/firewall/ipset/github -cidr {ip}'
-    os.system(cmd) # !! opt for subprocesses instead !!
+    cmd = ["pvesh", "create", "/cluster/firewall/ipset/github", "-cidr", ip]
+    subprocess.run(cmd, capture_output=True, text=True, check=True)
